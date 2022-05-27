c7a83c8492123571df867d4fc6d71c92599dfdc0
eddgalan <edd.galan@hotmail.com>
2022-05-27 16:53:20 -0500

    Logic Buttons - Se programa la lógica de los botones

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
 import './bootstrap/bootstrap.min.css';
+import { useState } from 'react';
 import ButtonReact from './components/button/button.jsx';
+import Counter from './components/counter/counter.jsx';
 import logoAlphaTeamDev from './img/AlphaTeamDev-sm.png';
 
 function App() {
+  const [numClicks, setNumClicks] = useState(0);
+
   const add = () => {
-    console.log("Click button");
+    setNumClicks( numClicks+1 );
   }
 
   const restart = () => {
-    console.log("Restart");
+    setNumClicks( 0 );
   }
 
   return (
@@ -18,6 +22,7 @@ function App() {
         <img className="logo" src={ logoAlphaTeamDev } alt="Logo AlphaTeamDev"/>
       </div>
       <div className="main-container">
+        <Counter numClicks={ numClicks } />
         <ButtonReact text="Clic" type="btn btn-primary" actionClick={add} />
         <ButtonReact text="Restart" type="btn btn-danger" actionClick={restart} />
       </div>
